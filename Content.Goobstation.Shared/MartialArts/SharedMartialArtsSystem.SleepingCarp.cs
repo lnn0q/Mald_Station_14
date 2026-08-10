816bc9c0dd6321436a5801f6e4fa59efb2b16b73
Sprinkle <some@test.com>
2026-08-10 22:00:52 +0300

    Revert "Merge pull request #444 from PerstronzioDesantis/carp-still"
    This reverts commit 35296f7dce568149e6376f77f6df8d4e1bba62e9, reversing
    changes made to fa5228df0591931583ca4ab415b5b8e88bffb9cf.

--- Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.SleepingCarp.cs
+++ Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.SleepingCarp.cs
@@ -92,8 +92,6 @@ public partial class SharedMartialArtsSystem
                 userReflect.Examinable = false; // no doxxing scarp users by examining lmao
                 userReflect.ReflectProb = 1;
                 userReflect.Spread = 60;
-                // Ratbite nerf
-                userReflect.ReflectionOnStandingStill = true;
                 Dirty(args.User, userReflect);
                 _popupSystem.PopupEntity(
                     Loc.GetString("carp-scroll-complete"),
--- Content.Shared/Weapons/Reflect/ReflectComponent.cs
+++ Content.Shared/Weapons/Reflect/ReflectComponent.cs
@@ -141,10 +141,6 @@ public sealed partial class ReflectComponent : Component
     [DataField, AutoNetworkedField]
     public float DamageOnReflectModifier;
     // WD END
-
-    // Ratbite
-    [DataField, AutoNetworkedField]
-    public bool ReflectionOnStandingStill = false;
 }
 
 [Flags, Serializable, NetSerializable]
